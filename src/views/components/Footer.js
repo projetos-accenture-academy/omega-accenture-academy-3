29d012519486df8a890f6330dfb842ab215a8a07
Raul Castro <raulc27@gmail.com>
2021-01-31 22:41:39 -0300

    tentando centralizar o footer

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -1,14 +1,16 @@
 let Footer = {
     render : async () => {
         let view = `
-        <footer class="container pt-md-5 pb-md-5 border-top mt-5 footer " style="  position: absolute;
+        <footer class="container pt-md-5 pb-md-5 border-top mt-5 footer  " 
+        
+        style="position: absolute;
         bottom: 0;
         width: 100%;
         height: 60px; 
         line-height: 60px; " >
-          <div class="d-flex justify-content-center">
-            Copyright ÔmegaBank© 2021 - Todos os direitos reservados
-          </div>
+        <div class="container">
+          <p class="text-center ">  Copyright ÔmegaBank© 2021 - Todos os direitos reservados </p>
+      </div>
         </footer>
           `
 
