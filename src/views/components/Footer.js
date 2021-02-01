0bbc22596ccf14f2865b06d23cb1aced7bf671c8
Raul Castro <raulc27@gmail.com>
2021-01-31 22:32:20 -0300

    consertando o footer (deixando ele la embaixo)

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -1,7 +1,11 @@
 let Footer = {
     render : async () => {
         let view = `
-        <footer class="container pt-md-5 pb-md-5 border-top mt-5 ">
+        <footer class="container pt-md-5 pb-md-5 border-top mt-5 footer " style="  position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 60px; 
+        line-height: 60px; " >
           <div class="d-flex justify-content-center">
             Copyright ÔmegaBank© 2021 - Todos os direitos reservados
           </div>
